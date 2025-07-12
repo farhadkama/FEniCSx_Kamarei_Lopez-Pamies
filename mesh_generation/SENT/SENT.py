c58f4f4d9ba4bb968d07acbd35d989a0c4d861cf
farhadkama <farhadkamarei@yahoo.com>
2025-07-12 23:44:29 +0000

    Add SENT mesh code

---
+++ mesh_generation/SENT/SENT.py
@@ -0,0 +1,104 @@
+from mpi4py import MPI
+from dolfinx import mesh, fem, io, plot, nls, log, geometry, la
+import basix.ufl
+import dolfinx.fem.petsc
+import ufl
+import numpy as np
+from petsc4py.PETSc import ScalarType
+from petsc4py import PETSc
+import time
+import os
+import gmsh
+
+
+comm = MPI.COMM_WORLD
+rank = comm.Get_rank()
+size = comm.Get_size()
+
+
+# Parameters for the outer rectangle
+
+
+
+L = 25/2
+
+H = 5
+B = 0.25
+
+ac = 1
+
+eps = 0.08
+h = 0.005
+
+make_mesh = True
+
+if make_mesh:
+    gmsh.initialize()
+    if MPI.COMM_WORLD.rank == 0:
+        gmsh.model.add("SENT")
+
+
+        # Create outer box
+        block = gmsh.model.occ.addBox(0, 0, 0, H, L, -B)
+
+        # Synchronize to reflect the changes in the model
+        gmsh.model.occ.synchronize()
+
+        # Add physical group for the volume (the tube itself)
+        gmsh.model.addPhysicalGroup(3, [block], 1)
+        gmsh.model.setPhysicalName(3, 1, "block")
+        
+
+
+        # Define mesh size fields
+        field_id = gmsh.model.mesh.field.add("Box")
+        gmsh.model.mesh.field.setNumber(field_id, "VIn", h)
+        gmsh.model.mesh.field.setNumber(field_id, "VOut", 32 * h)
+        gmsh.model.mesh.field.setNumber(field_id, "XMin", 0)
+        gmsh.model.mesh.field.setNumber(field_id, "XMax", 2*H/3)
+        gmsh.model.mesh.field.setNumber(field_id, "YMin",0)
+        gmsh.model.mesh.field.setNumber(field_id, "YMax", 1)
+        gmsh.model.mesh.field.setNumber(field_id, "ZMin", -B)
+        gmsh.model.mesh.field.setNumber(field_id, "ZMax", 0)
+
+
+        field2_id = gmsh.model.mesh.field.add("Box")
+        gmsh.model.mesh.field.setNumber(field2_id, "VIn", 4 * h)
+        gmsh.model.mesh.field.setNumber(field2_id, "VOut", 32 * h)
+        gmsh.model.mesh.field.setNumber(field2_id, "XMin", 0)
+        gmsh.model.mesh.field.setNumber(field2_id, "XMax", H)
+        gmsh.model.mesh.field.setNumber(field2_id, "YMin", 0)
+        gmsh.model.mesh.field.setNumber(field2_id, "YMax", L/3)
+        gmsh.model.mesh.field.setNumber(field2_id, "ZMin", -B)
+        gmsh.model.mesh.field.setNumber(field2_id, "ZMax", 0)
+
+
+        field3_id = gmsh.model.mesh.field.add("Box")
+        gmsh.model.mesh.field.setNumber(field3_id, "VIn", 16* h)
+        gmsh.model.mesh.field.setNumber(field3_id, "VOut", 32 * h)
+        gmsh.model.mesh.field.setNumber(field3_id, "XMin", 0)
+        gmsh.model.mesh.field.setNumber(field3_id, "XMax", H)
+        gmsh.model.mesh.field.setNumber(field3_id, "YMin", 0)
+        gmsh.model.mesh.field.setNumber(field3_id, "YMax", L/2)
+        gmsh.model.mesh.field.setNumber(field3_id, "ZMin", -B)
+        gmsh.model.mesh.field.setNumber(field3_id, "ZMax", 0)
+
+
+
+        # Combine the fields
+        min_field_id = gmsh.model.mesh.field.add("Min")
+        gmsh.model.mesh.field.setNumbers(min_field_id, "FieldsList", [field_id, field2_id, field3_id])
+        gmsh.model.mesh.field.setAsBackgroundMesh(min_field_id)
+
+        # Generate and optimize the mesh
+        gmsh.model.mesh.generate(3)
+        gmsh.model.mesh.optimize("Netgen")
+        gmsh.write("SENT.msh")
+
+    model = MPI.COMM_WORLD.bcast(gmsh.model, root=0)
+    partitioner = dolfinx.cpp.mesh.create_cell_partitioner(dolfinx.mesh.GhostMode.shared_facet)
+    mesh_data = io.gmshio.model_to_mesh(model, MPI.COMM_WORLD, 0, gdim=3, partitioner=partitioner)
+
+    gmsh.finalize()
+    MPI.COMM_WORLD.barrier()
+    domain = mesh_data[0]
